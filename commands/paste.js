abf7a44a6477f20ad75820d682816d0479c94a1b
TheWither125 <69727034+TheWither125@users.noreply.github.com>
2020-08-15 18:36:28 +0200

    First

---
+++ commands/paste.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'paste',
+    description: "paste your message",
+    execute(message, args){
+
+        if(message.member.roles.cache.has('737804239778349056')){
+            message.channel.send(message) 
+
+
+        } else {
+            message.channel.send('No permission');
+        }
+        
+
+    }        
+}
---
+++ commands/youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'youtube',
+    description: "youtube",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCNqqvBzF5-VwXyZ0wkpnpag');
+    }
+}
